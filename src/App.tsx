056ed4986fb319c507a2d72d7d64ef81fba61bef
filip.zwolinski <filip.zwolinski@riskmethods.net>
2022-11-13 01:49:37 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React TEST EST
+          Learn React TASDASDAS
         </a>
       </header>
     </div>
